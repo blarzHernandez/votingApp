e681c1c11664248cd1761288fc7e825aa65dce6d
Blarz hdz <info.redpaz@gmail.com>
2017-05-29 21:53:13 -0600

    candidate board

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Voting applicationr</title>
+	<title>Voting Application</title>
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="assets/css/bootstrap.min.css" >
 	<script src="assets/js/jquery-3.2.1.min.js"></script>
--- application/views/voting_main.php
+++ application/views/voting_main.php
@@ -5,10 +5,24 @@ foreach ($committe as $value){
 ?>
 <div class="panel panel-danger">
   <div class="panel-heading">
-    <h3 class="panel-title"></h3>
+    <h3 class="panel-title"><?php echo $value;?></h3>
   </div>
   <div class="panel-body">
-   <?php echo $value;?>
+    <?php 
+    $candidatesArr = array(
+      array("name"=>"Roberto Hernandez","country"=>"ES","committe"=>"PST","photo"=>"assets/images/candidate.jpg"),
+      array("name"=>"Roberto Blarz","country"=>"ES","committe"=>"PST","photo"=>"assets/images/candidate.jpg"),
+      array("name"=>"Blarz Hernandez","country"=>"ES","committe"=>"PvT","photo"=>"assets/images/candidate.jpg")
+      );
+    foreach ($candidatesArr as $key => $val) {
+    ?>
+      <div class="well col-sm-4">
+        <img src="<?php echo $val["photo"];?>" alt="<?php echo 'title';?>" class="img-rounded">
+      </div>
+    <?php
+        }
+    ?>
+
   </div>
 </div>
 <?php
